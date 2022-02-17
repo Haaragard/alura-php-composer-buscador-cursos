b1421b2b2c58afa1e59e76f8cc098a5fd6297653
Haaragard <diogololz159@gmail.com>
2022-02-16 21:57:40 -0300

    added .phan/config.php for code analysis config

---
+++ .phan/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'target_php_version' => '8.1',
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle',
+        'vendor/psr/http-message'
+    ],
+    'exclude_analysis_directory_list' => [
+        'vendor/'
+    ]
+];
